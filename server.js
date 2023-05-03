b8a76565d5b2fd5c7a158eb271f1128e7948debc
Jeff Jiang <jeff.jiang13@gmail.com>
2023-05-03 16:01:16 -0400

    first commit

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-
+import  Path from "path";
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = Path.dirname(__filename);
+
 
 //rest object
 const app = express();
